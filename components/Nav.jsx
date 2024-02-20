bd17cc471f6c79de4e7a9b5ba26717b86510350f
Josh May <josh.may.one@gmail.com>
2024-02-19 16:09:29 -0800

    Fleshing out nav bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,8 +7,17 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => 
 {
-
   const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+  useEffect(() => 
+  {
+    const setProviders = async () => 
+    {
+      const providers = await getProviders();
+      setProviders(providers);
+    }
+    setProviders();
+  }, []);
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
@@ -21,9 +30,19 @@ const Nav = () =>
         { isUserLoggedIn ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href="/create-prompt" className='black_btn'>Create Post</Link>
+            <button type='button' onClick={signOut} className='outline_btn'>Sign Out</button>
+            <Link href='/profile'>
+              <Image src="/assets/images/logo.svg" width={37} height={37} className='rounded-full' alt='profile' />
+            </Link>
           </div>
-        ): (<> </>)}
+        ): (
+        <>
+        {providers && Object.values(providers).map(provider => (
+          <button type='button' onClick={() => signIn(provider.id)} key={provider.name} className='black_btn'>Sign In</button>
+        ))}
 
+         </>
+         )}
       </div>
     </nav>
   )
