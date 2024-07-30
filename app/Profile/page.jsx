cbea153f64471ec4e65f89a08f9fc1a6e7364237
Josh May <josh.may.one@gmail.com>
2024-07-29 17:05:36 -0700

    Creating the profile component and route

--- app/Profile/page.jsx
+++ app/Profile/page.jsx
@@ -3,17 +3,34 @@ import { useState, useEffect } from "react";
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import Profile from '@components/Profile';
+import { data } from "autoprefixer";
 
 const MyProfile = () =>
 {
+    const { data: session } = useSession();
+
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = async () =>
+    {
+        const response = await fetch('/api/users/${session.user.id}/posts');
+        const data = await response.json();
+        setPosts(data);
+    }
+
+    useEffect(() =>
+    {
+        if (session?.user.id) fetchPosts();
+    }, []);
+
     const handleEdit = () => { };
     const handleDelete = async () => { };
 
     return (
         <Profile
-            name='Mine'
+            name='My'
             description='Welcome to your personalized profile page'
-            data={[]}
+            data={[data]}
             handleEdit={handleEdit}
             handleDelete={handleDelete}
         />
